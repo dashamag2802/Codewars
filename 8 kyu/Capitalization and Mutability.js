e8db5481f0d0c1590de54396e3c2776dcac7f646
dashamag2802 <dashamag@bk.ru>
2020-11-03 21:34:57 -0500

    add solution for Capitalization and Mutability update

--- 8 kyu/Capitalization and Mutability.js
+++ 8 kyu/Capitalization and Mutability.js
@@ -10,3 +10,10 @@ function capitalizeWord(word) {
     word[0].toUpperCase();
     return word;
 }
+
+
+
+function capitalizeWord(word) {
+    word = word[0].toUpperCase() + word.slice(1);
+    return word;
+}
