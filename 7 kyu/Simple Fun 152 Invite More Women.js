a9963089888cf44e4d706dce1f5962fc88dfa92d
dashamag2802 <dashamag@bk.ru>
2020-11-12 21:36:16 -0500

    Added solution for Simple Fun 152 Invite More Women

---
+++ 7 kyu/Simple Fun 152 Invite More Women.js
@@ -0,0 +1,15 @@
+/*
+Task
+King Arthur and his knights are having a New Years party. Last year Lancelot was jealous of Arthur, because Arthur had a date and Lancelot did not, and they started a duel.
+
+To prevent this from happening again, Arthur wants to make sure that there are at least as many women as men at this year's party. He gave you a list of integers of all the party goers.
+
+Arthur needs you to return true if he needs to invite more women or false if he is all set.
+ */
+function inviteMoreWomen(L) {
+    let sum = 0;
+    for (let i = 0; i < L.length; i++){
+        sum += L[i];
+    }
+    return sum > 0;
+}
