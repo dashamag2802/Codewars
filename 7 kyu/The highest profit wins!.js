adc87f0adef4a61a9112377c6352ab5b90886b7f
dashamag2802 <dashamag@bk.ru>
2020-11-12 21:28:25 -0500

    Added solution for The highest profit wins!

---
+++ 7 kyu/The highest profit wins!.js
@@ -0,0 +1,9 @@
+/*
+ask
+Write a function that returns both the minimum and maximum number of the given list/array.
+ */
+function minMax(arr){
+    let min = Math.min(...arr);
+    let max = Math.max(...arr);
+    return [min,max]; // fix me!
+}
