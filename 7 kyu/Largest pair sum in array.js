9ceacde17da8e4a93a239529d8aae61932a33d70
dashamag2802 <dashamag@bk.ru>
2020-11-19 20:21:12 -0500

    Added solution for Largest pair sum in array

---
+++ 7 kyu/Largest pair sum in array.js
@@ -0,0 +1,8 @@
+/*
+Given a sequence of numbers, find the largest pair sum in the sequence.
+ */
+function largestPairSum(numbers)
+{
+    numbers.sort((a, b) => a - b);
+    return numbers[numbers.length - 1] + numbers[numbers.length - 2];
+}
