37620cde7ad0a40cbb38fd3c2d7d52af7edc426a
dashamag2802 <dashamag@bk.ru>
2020-12-08 22:09:08 -0500

    Added solution for Spacify

---
+++ 7 kyu/Spacify.js
@@ -0,0 +1,6 @@
+/*
+Modify the spacify function so that it returns the given string with spaces inserted between each character.
+ */
+function spacify(str) {
+    return str.split('').join(' ');
+}
